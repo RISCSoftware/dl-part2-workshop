8b192699fc0a28ee11894a8a25d24f260aa81513
Markus Steindl <markus.steindl@risc-software.at>
2024-09-25 19:43:31 +0200

    Update docker image name

--- docs/docker_cheatsheet.sh
+++ docs/docker_cheatsheet.sh
@@ -1,30 +1,29 @@
+# git stuff
 cd ~/repos
 git clone git@github.com:RISCSoftware/dl-part2-workshop.git
 cd dl-part2-workshop
 
-# docker pull huggingface/transformers-pytorch-gpu:latest
-
-docker build -t dl-workshop .; docker build -t dl-workshop-dev -f Dockerfile.dev .
 docker compose -p dl-workshop -f docker-compose.yml up -d --scale runtime-jupyter=1 --scale runtime=2
-docker image prune -f
 
 # login to registry
 docker login https://index.docker.io/v1/
 
-registry=registry.risc-software.at/risc_ds/risc_dse
-# registry=risclidse/dl-workshop-part2
+# image=registry.risc-software.at/risc_ds/risc_dse/dl-workshop
+image=risclidse/dl-workshop
 version=1.1
 
-docker build -t $registry/dl-workshop:$version .; docker tag $registry/dl-workshop:$version $registry/dl-workshop:latest
-docker build -t $registry/dl-workshop-dev:$version -f Dockerfile.dev .; docker tag $registry/dl-workshop-dev:$version $registry/dl-workshop-dev:latest
+docker build -t $image:$version .
+docker tag $image:$version $image:latest
+docker build -t $image:$version-dev -f Dockerfile.dev .
+docker tag $image:$version-dev $image:dev
 
-docker push $registry/dl-workshop:$version
-docker push $registry/dl-workshop:latest
-docker push $registry/dl-workshop-dev:$version
-docker push $registry/dl-workshop-dev:latest
+docker push $image:$version
+docker push $image:latest
+docker push $image:$version-dev
+docker push $image:dev
 
-docker pull $registry/dl-workshop
-docker pull $registry/dl-workshop-dev
+docker pull $image
+docker pull $image-dev
 
 docker exec -it dl-workshop-dev bash
 docker logs dl-workshop-dev
