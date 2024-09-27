144a01c12fbe64fa638f94f733baa7f7ad141f4e
Markus Steindl <markus.steindl@risc-software.at>
2024-09-27 14:21:56 +0200

    Add helper script for copying images

---
+++ bash/images.sh
@@ -0,0 +1,5 @@
+localdir=/mnt/c/Users/msteindl
+remotedir=/home/msteindl/repos/dl-part2-workshop/images
+
+scp $localdir/msteindl.jpg msteindl@qftquad2.risc.jku.at:$remotedir
+scp $localdir/eocansey.jpg msteindl@qftquad2.risc.jku.at:$remotedir
