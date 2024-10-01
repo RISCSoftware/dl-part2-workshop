fbe29dbca3507693bafdbef4fa69decaa1138ee8
Markus Steindl <markus.steindl@risc-software.at>
2024-10-01 13:24:22 +0200

    Cleanup

--- bash/images.sh
+++
@@ -1,5 +0,0 @@
-localdir=/mnt/c/Users/msteindl
-remotedir=/home/msteindl/repos/dl-part2-workshop/images
-
-scp $localdir/msteindl.jpg msteindl@qftquad2.risc.jku.at:$remotedir
-scp $localdir/eocansey.jpg msteindl@qftquad2.risc.jku.at:$remotedir
