93375c907e919b05c93e358d5e74d2d740b565cf
Markus Steindl <markus.steindl@risc-software.at>
2024-12-04 13:19:09 +0100

    Update docker and user config files

---
+++ bash/create_presenter_dirs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Read the list of workshop users
+mapfile -t users < /repo/presenters.list
+
+# Create a home directory for each user
+for user in "${users[@]}"; do
+  mkdir -p "/home/$user"
+  cp -r "/repo" "/home/$user"
+done
